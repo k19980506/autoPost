03b108e708e90b06283501c0f584f9629d447dba
King <k19980506@gmail.com>
2024-04-29 19:04:33 +0800

    Updates

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import PostScheduler from "./PostScheduler";
+import Facebook from "./Facebook";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PostScheduler /> */}
+      <Facebook />
+    </div>
+  );
+}
+
+export default App;
